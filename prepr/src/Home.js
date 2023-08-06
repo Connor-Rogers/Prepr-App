f6ce42796a72ebceb563998652e45a73558a1b4b
tristam-sanborn <sanbornt1@wit.edu>
2023-08-06 12:16:20 -0400

    moved pantry modal off of home page and new home

--- prepr/src/Home.js
+++ prepr/src/Home.js
@@ -1,131 +1,47 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import reactModal from 'react-modal';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from './firebase';
-
-import './Home.css';
 
 export const Home = () => {
   const { user } = useAuthState();
+  const [recipes, setRecipes] = useState(null);
 
-  const history = useHistory();
-  const [mealPlan, setMealPlan] = useState(null);
-  const [likes, setLikes] = useState({});
-  const [pantryItems, setPantryItems] = useState([]);
-  const [newPantryItem, setNewPantryItem] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [days, setDays] = useState('');
-
-  const handleLoginClick = () => {
-    history.push('/auth');
-  };
-
-  const handleGenerateMealPlanClick = () => {
-    setShowModal(true);
-  };
-
-  const handleMealPlanSubmit = async () => {
-    const pantryItemsData = {
-      pantryItems,
-      days,
-    };
-
-    // Get the id token
+  const fetchRecipes = async () => {
     const idToken = await user.getIdToken();
-
-    const response = await fetch(`http://127.0.0.1:5000/gen`, {
-
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${idToken}`, // Include the Authorization header
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(pantryItemsData),
-    });
-
-    const data = await response.json();
-    setMealPlan(data);
-    setLikes({});  // Reset likes/dislikes when fetching new meal plan
-
-    setShowModal(false);
-  };
-
-
-
-  const handleGenerateNewRecipe = async (day) => {
-    const pantryItemsData = {
-      pantryItems,
-    };
-    const idToken = await user.getIdToken();
-
-    const response = await fetch(`http://127.0.0.1:5000/genSingle`, {
-      method: 'POST',
+    const response = await fetch(`http://127.0.0.1:5000/gen3`, { // call your endpoint
+      method: 'POST', // use a POST request
       headers: {
         'Authorization': `Bearer ${idToken}`,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(pantryItemsData),
     });
 
     const data = await response.json();
-    setMealPlan(prevMealPlan => ({ ...prevMealPlan, [day]: data }));
+    setRecipes(data);
   };
 
-  const handleMealCardClick = (meal) => {
-    history.push(`/meal/${meal.id}`);  // assuming your meals have an 'id' field
-  };
-
-  const handleDislike = (day) => {
-    setLikes({ ...likes, [day]: 'dislike' });
-    handleGenerateNewRecipe(day);
-  };
-
-  const handlePantryItemSubmit = () => {
-    setPantryItems([...pantryItems, newPantryItem]);
-    setNewPantryItem('');
-  };
-
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
 
   return (
-    <div className="app-container">
-      <div className="form-container">
-        <h1>Welcome to Prepr</h1>
-        <p>Your solution to easy meal planning!</p>
+    <div className="home-container">
+      <h1>Welcome to Prepr</h1>
+      <p>Your solution to easy meal planning!</p>
 
-        <button onClick={handleGenerateMealPlanClick}>Generate Meal Plan</button>
-
-        {showModal && <div>
-          <h3>Meal Plan Options</h3>
-          <input type="number" value={days} onChange={(e) => setDays(e.target.value)} min="1" max="7" placeholder="Number of days" />
-
-          <h3>Pantry Items</h3>
-          <input type="text" value={newPantryItem} onChange={(e) => setNewPantryItem(e.target.value)} />
-          <button onClick={handlePantryItemSubmit}>+</button>
-
-          <ul>
-            {pantryItems.map((item, index) => <li key={index}>{item}</li>)}
-          </ul>
-
-          <button onClick={handleMealPlanSubmit}>Submit</button>
-        </div>}
-
-        {mealPlan && <div className="meal-plan-container">
-          <h2>Your Meal Plan</h2>
-          {Object.keys(mealPlan).map((day) => (
-            <div className={`meal-plan-day ${likes[day]}`} key={day}>
-              <h3>{day}</h3>
-              <p>{mealPlan[day]}</p>
-              <div className="meal-plan-actions">
-                <button onClick={() => handleLike(day)}>✔️</button>
-                <button onClick={() => handleDislike(day)}>❌</button>
-              </div>
+      {recipes ? (
+        <div className="recipe-grid">
+          {recipes.map((recipe, index) => (
+            <div className="recipe-card" key={index}>
+              <h2>{recipe.title}</h2>
+              <p>{recipe.instructions}</p>
             </div>
           ))}
-        </div>}
-      </div>
+        </div>
+      ) : (
+        <p>Loading recipes...</p>
+      )}
     </div>
   );
-
 };
 
 export default Home;
