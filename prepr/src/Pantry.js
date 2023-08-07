d0c906ba1e39873525d77adcf0080b4eda20b782
tristam-sanborn <sanbornt1@wit.edu>
2023-08-07 16:31:35 -0400

    redid pantry.js

--- prepr/src/Pantry.js
+++ prepr/src/Pantry.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { useAuthState } from './firebase';
 import './Pantry.css';
-
+import { useHistory } from 'react-router-dom';
 
 export const Pantry = () => {
+  const history = useHistory();
   const { user } = useAuthState();
   const [newPantryItem, setNewPantryItem] = useState('');
   const [pantryItems, setPantryItems] = useState([]);
@@ -22,13 +23,12 @@ export const Pantry = () => {
       days,
     };
 
-    // Get the id token
     const idToken = await user.getIdToken();
 
     const response = await fetch(`http://127.0.0.1:5000/gen3`, {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${idToken}`, // Include the Authorization header
+        'Authorization': `Bearer ${idToken}`,
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(pantryItemsData),
@@ -37,6 +37,31 @@ export const Pantry = () => {
     const data = await response.json();
     setMealPlan(data);
     setLikes({});  // Reset likes/dislikes when fetching new meal plan
+  };
+
+  const addRecipe = async (recipe) => {
+    const idToken = await user.getIdToken();
+    const response = await fetch(`http://127.0.0.1:5000/gen/add`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${idToken}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: recipe.title,
+        instructions: recipe.instructions,
+        calories: recipe.calories,
+        fats: recipe.fats,
+        carbs: recipe.carbs,
+        proteins: recipe.proteins,
+        ingredients: recipe.ingredients
+      }),
+    });
+
+    const data = await response.json();
+    if (data.message === "Recipe created successfully") {
+      history.push(`/recipe/${data.recipe_id}`);
+    }
   };
 
   const handleGenerateNewRecipe = async (day) => {
@@ -67,25 +92,6 @@ export const Pantry = () => {
     setLikes({ ...likes, [day]: 'like' });
     handleGenerateNewRecipe(day);
   };
-//  const handleAddToMealPlan = async (recipeId) => {
-//    const idToken = await user.getIdToken();
-//    const recipeId = '123'; // Replace with the actual recipe ID
-//    const response = await fetch(`http://127.0.0.1:5000/meal_plan/${user.uid}/recipe/${recipeId}`, {
-//      method: 'POST',
-//      headers: {
-//        'Authorization': `Bearer ${idToken}`,
-//        'Content-Type': 'application/json'
-//      },
-//    });
-  
- //   const data = await response.json();
-//    if (response.ok) {
-//      // Update the state or do something else with the data
-//    } else {
-//      console.error(data.error);
-//    }
-//  };
-  
 
   return (
     <div>
@@ -105,18 +111,17 @@ export const Pantry = () => {
       {mealPlan && <div className="meal-plan-container">
         <h2>Your Meal Plan</h2>
         {Object.keys(mealPlan).map((day) => (
-        <div className={`meal-plan-day ${likes[day]}`} key={day}>
-          <h3>{day}</h3>
-          <p>{mealPlan[day]}</p>
-          <div className="meal-plan-actions">
-            <button onClick={() => handleLike(day)}>✔️</button>
-            <button onClick={() => handleDislike(day)}>❌</button>
-            {/*<button onClick={() => handleAddToMealPlan(recipeId)}>Add to Meal Plan</button> {/* Replace recipeId with the actual ID */}
-      </div>
-    </div>
-  ))}
-</div>}
-
+          <div className={`meal-plan-day ${likes[day]}`} key={day}>
+            <h3>{day}</h3>
+            <p>{mealPlan[day]}</p>
+            <div className="meal-plan-actions">
+              <button onClick={() => handleLike(day)}>✔️</button>
+              <button onClick={() => handleDislike(day)}>❌</button>
+              <button onClick={() => addRecipe(mealPlan[day])}>Add to Recipe</button>
+            </div>
+          </div>
+        ))}
+      </div>}
     </div>
   );
 };
